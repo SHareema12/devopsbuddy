9ab6a7bcd8ed5d4f4d63334eea8293823cc9cc5f
Safia Hareema <shareema12@gmail.com>
2018-09-23 22:37:19 -0400

    change gitignore file

--- src/main/java/com/devopsbuddy/devopsbuddy/DevopsbuddyApplication.java
+++ src/main/java/com/devopsbuddy/devopsbuddy/DevopsbuddyApplication.java
@@ -8,5 +8,8 @@ public class DevopsbuddyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevopsbuddyApplication.class, args);
+		System.out.println("Hello");
 	}
+
+
 }
